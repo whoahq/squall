4e9f05c178b22b9f9f59d27691ff66c230090772
fallenoak <git@fallenoak.me>
2020-09-08 21:37:30 -0500

    feat(string): add SStrCopy

---
+++ storm/String.cpp
@@ -0,0 +1,35 @@
+#include "String.hpp"
+#include "Error.hpp"
+
+size_t SStrCopy(char* dest, const char* source, size_t destsize) {
+    STORM_ASSERT(dest);
+    STORM_ASSERT(source);
+
+    char* destbuf = dest;
+
+    if (destsize == 0x7FFFFFFF) {
+        while (*source) {
+            *destbuf = *source;
+
+            ++destbuf;
+            ++source;
+        }
+    } else {
+        if (*source) {
+            while (destbuf < &dest[destsize - 1]) {
+                *destbuf = *source;
+
+                ++destbuf;
+                ++source;
+
+                if (!*source) {
+                    *destbuf = '\0';
+                    return static_cast<size_t>(destbuf - dest);
+                }
+            }
+        }
+    }
+
+    *destbuf = '\0';
+    return static_cast<size_t>(destbuf - dest);
+}
---
+++ storm/String.hpp
@@ -0,0 +1,8 @@
+#ifndef STORM_STRING_HPP
+#define STORM_STRING_HPP
+
+#include <cstdlib>
+
+size_t SStrCopy(char* dest, const char* source, size_t destsize);
+
+#endif
