edc066f4965189652ab4a66473d0710efee62992
fallenoak <git@fallenoak.me>
2020-09-21 22:47:23 -0500

    chore(test): add additional list tests

--- test/List.cpp
+++ test/List.cpp
@@ -23,4 +23,47 @@ TEST_CASE("TSList::LinkToHead", "[list]") {
 
         delete node;
     }
+
+    SECTION("links node to tail correctly") {
+        STORM_LIST(TestListNode) list;
+
+        auto node = new TestListNode();
+        list.LinkToTail(node);
+
+        REQUIRE(list.Tail() == node);
+
+        delete node;
+    }
+
+    SECTION("links nodes to head and tail correctly") {
+        STORM_LIST(TestListNode) list;
+
+        auto node1 = new TestListNode();
+        list.LinkToHead(node1);
+
+        auto node2 = new TestListNode();
+        list.LinkToTail(node2);
+
+        REQUIRE(list.Head() == node1);
+        REQUIRE(list.Tail() == node2);
+
+        delete node1;
+        delete node2;
+    }
+
+    SECTION("links multiple nodes to head correctly") {
+        STORM_LIST(TestListNode) list;
+
+        auto node1 = new TestListNode();
+        list.LinkToHead(node1);
+
+        auto node2 = new TestListNode();
+        list.LinkToHead(node2);
+
+        REQUIRE(list.Head() == node2);
+        REQUIRE(list.Tail() == node1);
+
+        delete node1;
+        delete node2;
+    }
 }
