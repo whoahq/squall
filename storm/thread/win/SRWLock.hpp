2fb9fd284c9aac69c380a22d5755bbe736e40f47
fallenoak <git@fallenoak.me>
2020-12-01 17:40:37 -0600

    feat(thread): implement CSRWLock::Enter and CSRWLock::Leave for windows (#1)

---
+++ storm/thread/win/SRWLock.hpp
@@ -0,0 +1,26 @@
+#ifndef STORM_THREAD_WIN_S_RW_LOCK_HPP
+#define STORM_THREAD_WIN_S_RW_LOCK_HPP
+
+#include <cstdint>
+
+class SRWLock {
+    public:
+    // Types
+    struct SUNNLOCK {
+        int32_t m_state;
+        int32_t m_event;
+    };
+
+    struct SURWLOCK {
+        SRWLock::SUNNLOCK m_mutex;
+        int32_t m_readerEvent;
+    };
+
+    // Static functions
+    static void SUNNLockEnter(volatile SUNNLOCK* sunnlock);
+    static void SUNNLockLeave(volatile SUNNLOCK* sunnlock);
+    static void SURWLockEnter(volatile SURWLOCK* surwlock, int32_t forwriting);
+    static void SURWLockLeave(volatile SURWLOCK* surwlock, int32_t fromwriting);
+};
+
+#endif
