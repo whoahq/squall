34e01acca5639f0d64212d8f20b253958451d243
fallenoak <git@fallenoak.me>
2021-08-09 01:03:37 -0500

    feat(thread): add SSemaphore::Signal

--- storm/thread/SSemaphore.cpp
+++ storm/thread/SSemaphore.cpp
@@ -13,3 +13,31 @@ SSemaphore::SSemaphore(uint32_t initialCount, uint32_t maximumCount)
     pthread_cond_init(&this->m_cond, 0);
 #endif
 }
+
+int32_t SSemaphore::Signal(uint32_t count) {
+#if defined(WHOA_SYSTEM_WIN)
+    return ReleaseSemaphore(this->m_opaqueData, count, nullptr);
+#endif
+
+#if defined(WHOA_SYSTEM_MAC) || defined(WHOA_SYSTEM_LINUX)
+    pthread_mutex_lock(&this->m_mutex);
+
+    if (this->m_value1 + count > this->m_value2) {
+        pthread_mutex_unlock(&this->m_mutex);
+
+        return 0;
+    }
+
+    this->m_value1 += count;
+
+    if (count <= 1) {
+        pthread_cond_signal(&this->m_cond);
+    } else {
+        pthread_cond_broadcast(&this->m_cond);
+    }
+
+    pthread_mutex_unlock(&this->m_mutex);
+
+    return 1;
+#endif
+}
--- storm/thread/SSemaphore.hpp
+++ storm/thread/SSemaphore.hpp
@@ -8,6 +8,7 @@ class SSemaphore : public SSyncObject {
     public:
     // Member functions
     SSemaphore(uint32_t initialCount, uint32_t maximumCount);
+    int32_t Signal(uint32_t count);
 };
 
 #endif
