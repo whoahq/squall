8521485e4947414be297ab3c29c8743e7ba399c8
fallenoak <git@fallenoak.me>
2022-12-29 14:56:12 -0600

    feat(thread): implement S_Thread::s_SLaunchThread for windows

--- storm/thread/win/S_Thread.cpp
+++ storm/thread/win/S_Thread.cpp
@@ -1,6 +1,24 @@
 #include "storm/thread/S_Thread.hpp"
+#include "storm/Memory.hpp"
 
 DWORD S_Thread::s_SLaunchThread(void* threadParam) {
+    auto params = static_cast<SThreadParmBlock*>(threadParam);
+    auto proc = params->threadProc;
+    auto param = params->threadParam;
+
+    SMemFree(threadParam);
+
+    auto val = proc(param);
+
     // TODO
-    return 0;
+    // S_Thread::s_threadCrit.Enter();
+
+    // TODO
+    // - remove tracking in S_Thread::s_threads
+    // - decrement num threads
+
+    // TODO
+    // S_Thread::s_threadCrit.Leave();
+
+    return val;
 }
