6ecc5c7bc228db93d564cdc297fe054c0530ff44
fallenoak <git@fallenoak.me>
2022-12-28 22:26:33 -0600

    feat(thread): implement S_Thread::s_SLaunchThread for linux

--- storm/thread/linux/S_Thread.cpp
+++ storm/thread/linux/S_Thread.cpp
@@ -3,4 +3,24 @@
 
 void* S_Thread::s_SLaunchThread(void* threadParam) {
     // TODO
+
+    auto params = static_cast<SThreadParmBlock*>(threadParam);
+
+    // TODO
+
+    params->threadProc(params->threadParam);
+
+    // TODO
+
+    if (params->syncObject) {
+        pthread_mutex_lock(&params->syncObject->m_mutex);
+        params->syncObject->m_value1 = 1;
+        pthread_mutex_unlock(&params->syncObject->m_mutex);
+
+        pthread_cond_signal(&params->syncObject->m_cond);
+    }
+
+    SMemFree(threadParam);
+
+    return nullptr;
 }
