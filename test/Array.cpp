c0637fc0c60c6fb706f51f465283e38ab55a666d
fallenoak <git@fallenoak.me>
2023-01-15 16:46:58 -0600

    feat(array): add TSBaseArray::Top

--- test/Array.cpp
+++ test/Array.cpp
@@ -30,6 +30,29 @@ TEST_CASE("TSFixedArray", "[array]") {
     }
 }
 
+TEST_CASE("TSFixedArray::Top", "[array]") {
+    SECTION("returns nullptr when array has no elements") {
+        TSFixedArray<uint32_t> array;
+        array.SetCount(0);
+        REQUIRE(array.Top() == nullptr);
+    }
+
+    SECTION("returns first element when array has 1 element") {
+        TSFixedArray<uint32_t> array;
+        array.SetCount(1);
+        array[0] = 1000;
+        REQUIRE(*array.Top() == 1000);
+    }
+
+    SECTION("returns last element when array has 2 elements") {
+        TSFixedArray<uint32_t> array;
+        array.SetCount(2);
+        array[0] = 1000;
+        array[1] = 1001;
+        REQUIRE(*array.Top() == 1001);
+    }
+}
+
 TEST_CASE("TSGrowableArray", "[array]") {
     SECTION("constructs correctly") {
         TSGrowableArray<uint32_t> array;
