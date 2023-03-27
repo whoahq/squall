0037f3e24427cef0f61fa484c94d905438bb3fcc
fallenoak <git@fallenoak.me>
2023-03-26 21:19:17 -0500

    feat(hash): add unsynchronized export table template

--- storm/Hash.hpp
+++ storm/Hash.hpp
@@ -2,6 +2,7 @@
 #define STORM_HASH_HPP
 
 #include "storm/hash/Hashkey.hpp"
+#include "storm/hash/TSExportTableSimple.hpp"
 #include "storm/hash/TSHashObject.hpp"
 #include "storm/hash/TSHashTable.hpp"
 #include "storm/hash/TSHashTableReuse.hpp"
