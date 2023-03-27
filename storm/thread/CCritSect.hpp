70495d86fd4e5af3545f759ca378bdab8ec7b38f
fallenoak <git@fallenoak.me>
2023-03-26 22:50:01 -0500

    feat(thread): add CCritSect

---
+++ storm/thread/CCritSect.hpp
@@ -0,0 +1,31 @@
+#ifndef STORM_THREAD_C_CRIT_SECT_HPP
+#define STORM_THREAD_C_CRIT_SECT_HPP
+
+#if defined(WHOA_SYSTEM_WIN)
+#include <windows.h>
+#endif
+
+#if defined(WHOA_SYSTEM_MAC) || defined(WHOA_SYSTEM_LINUX)
+#include <pthread.h>
+#endif
+
+class CCritSect {
+    public:
+    // Member functions
+    CCritSect();
+    ~CCritSect();
+    void Enter();
+    void Leave();
+
+    private:
+    // Member variables
+#if defined(WHOA_SYSTEM_WIN)
+    CRITICAL_SECTION m_critsect;
+#endif
+
+#if defined(WHOA_SYSTEM_MAC) || defined(WHOA_SYSTEM_LINUX)
+    pthread_mutex_t m_critsect;
+#endif
+};
+
+#endif
