e9e0f91b524777cb09be48cb8d6f0527c09db8f8
fallenoak <git@fallenoak.me>
2025-09-03 20:38:06 -0500

    feat(hash): finish implementation of HASHKEY classes

--- storm/hash/Hashkey.hpp
+++ storm/hash/Hashkey.hpp
@@ -1,41 +1,52 @@
 #ifndef STORM_HASH_HASHKEY_HPP
 #define STORM_HASH_HASHKEY_HPP
 
+class HASHKEY_NONE {
+    public:
+    // Member functions
+    bool operator==(const HASHKEY_NONE& key);
+};
+
 class HASHKEY_PTR {
     public:
-    // Member variables
-    void* m_key;
-
     // Member functions
     HASHKEY_PTR();
     HASHKEY_PTR(void* key);
+    HASHKEY_PTR& operator=(const HASHKEY_PTR& key);
     bool operator==(const HASHKEY_PTR& key) const;
+    void* GetPtr() const;
+
+    private:
+    // Member variables
+    void* m_key;
 };
 
 class HASHKEY_STR {
     public:
-    // Member variables
-    char* m_str;
-
     // Member functions
     HASHKEY_STR();
     HASHKEY_STR(const char* str);
     ~HASHKEY_STR();
     HASHKEY_STR& operator=(const char* str);
+    HASHKEY_STR& operator=(const HASHKEY_STR& key);
     bool operator==(const char* str) const;
+    bool operator==(const HASHKEY_STR& key) const;
+    const char* GetString() const;
+
+    protected:
+    // Member variables
+    char* m_str;
 };
 
 class HASHKEY_STRI : public HASHKEY_STR {
     public:
     // Member functions
+    HASHKEY_STRI() : HASHKEY_STR() {};
+    HASHKEY_STRI(const char* str) : HASHKEY_STR(str) {};
     HASHKEY_STRI& operator=(const char* str);
+    HASHKEY_STRI& operator=(const HASHKEY_STRI& key);
     bool operator==(const char* str) const;
-};
-
-class HASHKEY_NONE {
-    public:
-    // Member functions
-    bool operator==(const HASHKEY_NONE& key);
+    bool operator==(const HASHKEY_STRI& key) const;
 };
 
 #endif
