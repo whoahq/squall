931096bb5023fe95e049fd7f91aa799953129eb8
Adam Heinermann <aheinerm@gmail.com>
2025-11-15 01:35:07 -0800

    feat(unicode): add 8 SUni functions

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -28,6 +28,7 @@ To use a different version of Storm.dll, use the following arguments in the cmak
 |-----------|-------|
 |`-DWHOA_STORMDLL_VERSION=2016`| Starcraft 1.17 |
 |`-DWHOA_STORMDLL_VERSION=2007`| Starcraft 1.15.0 ~ 1.16.1 |
-|`-DWHOA_STORMDLL_VERSION=2003`| Starcraft 1.10 ~ 1.14.0, Diablo II 1.10 ~ 1.13d |
+|`-DWHOA_STORMDLL_VERSION=2004`| Starcraft 1.11b ~ 1.14 |
+|`-DWHOA_STORMDLL_VERSION=2003`| Starcraft 1.10 ~ 1.11, Diablo II 1.10 ~ 1.13d |
 |`-DWHOA_STORMDLL_VERSION=2000`| Diablo 1.09, Starcraft 1.08 ~ Starcraft 1.09b, Diablo II 1.06 ~ Diablo II 1.09d, Warcraft II: BNE 2.02.1 |
 |`-DWHOA_STORMDLL_VERSION=1999`| Diablo 1.08, Diablo II 1.0 ~ Diablo II 1.05b |
--- storm/Unicode.hpp
+++ storm/Unicode.hpp
@@ -2,6 +2,25 @@
 #define STORM_UNICODE_HPP
 
 #include "Core.hpp"
+#include <cstddef>
+
+ptrdiff_t STORMAPI SUniConvertUTF16ToDos(char* dest, const char16_t* source, uint32_t destsize);
+
+ptrdiff_t STORMAPI SUniConvertUTF16ToMac(char* dest, const char16_t* source, uint32_t destsize);
+
+ptrdiff_t STORMAPI SUniConvertUTF16ToWin(char* dest, const char16_t* source, uint32_t destsize);
+
+ptrdiff_t STORMAPI SUniConvertDosToUTF16(char16_t* dest, const char* source, uint32_t destsize);
+
+ptrdiff_t STORMAPI SUniConvertMacToUTF16(char16_t* dest, const char* source, uint32_t destsize);
+
+ptrdiff_t STORMAPI SUniConvertWinToUTF16(char16_t* dest, const char* source, uint32_t destsize);
+
+// Assumes index is within the bounds of utf8String
+int32_t STORMAPI SUniFindAfterUTF8Chr(const char* utf8String, int32_t index);
+
+// Assumes index is within the bounds of utf8String
+int32_t STORMAPI SUniFindUTF8ChrStart(const char* utf8String, int32_t index);
 
 uint32_t STORMAPI SUniSGetUTF8(const uint8_t* strptr, int32_t* chars);
 
