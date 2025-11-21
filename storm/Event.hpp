e6bb047adfa476c6f9a9bb242ae82594b0ab1758
fallenoak <git@fallenoak.me>
2025-11-20 21:36:18 -0600

    chore(style): clean up line endings in event

--- storm/Event.hpp
+++ storm/Event.hpp
@@ -4,9 +4,7 @@
 #include <cstdint>
 #include "Core.hpp"
 
-
 typedef void (STORMAPI* SEVTHANDLER)(void*);
-
 
 int32_t STORMAPI SEvtBreakHandlerChain(void* data);
 
@@ -23,6 +21,5 @@ int32_t STORMAPI SEvtRegisterHandler(uint32_t type, uint32_t subtype, uint32_t i
 int32_t STORMAPI SEvtUnregisterHandler(uint32_t type, uint32_t subtype, uint32_t id, SEVTHANDLER handler);
 
 int32_t STORMAPI SEvtUnregisterType(uint32_t type, uint32_t subtype);
-
 
 #endif
